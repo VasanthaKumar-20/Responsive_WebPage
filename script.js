7861c5aa13331712b6554bf34e3b10bf92d804a3
vasanth kumar <vasanthsanjee@gmail.com>
2024-11-19 22:59:30 +0530

    Responsive WebPage

---
+++ script.js
@@ -0,0 +1,4 @@
+// Add any JavaScript interactivity here, such as handling button clicks or modifying the carousel dynamically.
+document.querySelector(".btn-discover").addEventListener("click", () => {
+  alert("Discover the amazing Mini Cooper!");
+});
